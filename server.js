70998617d3efffbf4bd27eb5161c0932dc5907d2
piuskrisantono <pius.krisantono@gmail.com>
2020-06-09 15:09:40 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API Running')
+})
+
+app.listen(PORT, () => {
+  console.log("Server is up and running on port " + PORT)
+})
